aefaa60e2a4e738f6a47192e7d7c128c199cb731
zhengshengshi <zhengshengshi01@chinatopcredit.com>
2016-11-17 21:01:56 +0800

    add button component

---
+++ src/components/shared/Button.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+import { colors } from 'styles/varibles';
+
+export default class Button extends Component {
+  static propTypes = {
+    onPress: PropTypes.func,
+    text: PropTypes.string
+  };
+
+  render() {
+    let style = StyleSheet.flatten(this.props.style);
+    let { fontSize, color, lineHeight, ...btnStyle } = style;
+
+    return (
+      <TouchableOpacity onPress={this.props.onPress} style={[styles.btn, btnStyle]}>
+        {this._renderChildren({ fontSize, color, lineHeight })}
+      </TouchableOpacity>
+    );
+  }
+
+  _renderChildren(txtStyle) {
+    if(this.props.children) {
+      return this.props.children;
+    }
+
+    let style = {
+      color: txtStyle.color || txtDefaultStyle.color,
+      lineHeight: txtStyle.lineHeight || txtDefaultStyle.lineHeight,
+      fontSize: txtStyle.fontSize || txtDefaultStyle.fontSize
+    }
+
+    return (
+      <Text style={style}>{this.props.text}</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+const txtDefaultStyle = {
+  lineHeight: 50,
+  fontSize: 20,
+  color: '#fff'
+}
