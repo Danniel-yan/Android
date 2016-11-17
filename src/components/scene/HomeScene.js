a5a331d8ac4f830d1af0a253323d1e736530d644
zhengshengshi <zhengshengshi01@chinatopcredit.com>
2016-11-17 19:15:09 +0800

    首页header

--- src/components/scene/HomeScene.js
+++ src/components/scene/HomeScene.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar, Image, View, Text, StyleSheet, Platform } from 'react-native';
 
 import NavigationTest from 'components/NavigationTest';
 
-import iconHome from 'assets/tab-icons/home.png';
+import { colors } from 'styles/varibles'
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
 
 export default class HomeScene extends Component {
   render() {
     return (
-      <View style={{ paddingTop: 100, flex: 1, backgroundColor: 'yellow' }}>
+      <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+        <StatusBar barStyle="light-content"/>
+        {this._renderHeader()}
       </View>
     );
   }
+
+  _renderHeader() {
+    return (
+      <View style={styles.header}>
+        <View style={styles.left}><Image source={require('assets/icons/pin2.png')}/><Text style={styles.locTxt}>上海</Text></View>
+        <View style={styles.center}><Text style={styles.titleTxt}>钞市</Text></View>
+        <View style={styles.right}><Image source={require('assets/icons/message.png')}/></View>
+      </View>
+    )
+  }
 } 
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    height: 57,
+    paddingTop: STATUSBAR_HEIGHT,
+    backgroundColor: colors.primary,
+    alignItems: 'center'
+  },
+
+  locTxt: {
+    fontSize: 15,
+    marginLeft: 3,
+    color: '#fff'
+  },
+
+  titleTxt: {
+    fontSize: 18,
+    color: '#fff'
+  },
+
+  center: {
+    width: 100,
+    alignItems: 'center'
+  },
+
+  left: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: 10
+  },
+
+  right: {
+    flex: 1,
+    paddingRight: 10,
+    alignItems: 'flex-end'
+  }
+});
