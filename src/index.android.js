d2fa243f385c87a223f5dc5fe395380f07ee1ca7
zhengshengshi <zhengshengshi01@chinatopcredit.com>
2016-11-22 09:12:43 +0800

    android

--- src/index.android.js
+++ src/index.android.js
@@ -1,18 +1,39 @@
-
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet } from 'react-native';
+import { ActivityIndicator, AppRegistry, StyleSheet } from 'react-native';
 
-import { createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducers from 'reducers';
 
+import * as defaultStyles from 'styles';
 import ExternalNavigationContainer from 'containers/ExternalNavigationContainer';
+import { applicationSetup } from 'settings'
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
 
 export default class supermarketjs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { initialing: true };
+  }
+
+  componentDidMount() {
+    applicationSetup().then(() => {
+      this.setState({ initialing: false });
+    });
+  }
+
   render() {
+    if(this.initialing) {
+      return (
+        <ActivityIndicator style={[defaultStyles.container, defaultStyles.centering]} animating={true} />
+      );
+    }
+
     return (
       <Provider store={store}>
         <ExternalNavigationContainer/>
