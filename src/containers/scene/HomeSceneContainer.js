6855811efd31ef80f49f73df54180c69842e310a
王睆 <wanghuan01@chinatopcredit.com>
2016-11-25 18:06:41 +0800

    Update

--- src/containers/scene/HomeSceneContainer.js
+++ src/containers/scene/HomeSceneContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 
 import { majorPush, majorPop, externalPush, externalPop } from 'actions/navigation';
+import { setAmount } from 'actions/scene/fast/filterList';
 import HomeScene from 'components/scene/HomeScene';
 
 function mapStateToProps(state) {
@@ -10,6 +11,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
+    setAmount: amount => dispatch(setAmount(amount)),
     majorPush: route => dispatch(majorPush(route)),
     majorPop: route => dispatch(majorPop(route)),
     externalPush: route => dispatch(externalPush(route)),
