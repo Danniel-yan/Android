33edb5f10054ed7b2c43ed728e74174ac21b00b7
zhengshengshi <zhengshengshi01@chinatopcredit.com>
2016-11-29 17:38:52 +0800

    联系我们

--- src/components/scene/zone/ContactScene.js
+++ src/components/scene/zone/ContactScene.js
@@ -14,26 +14,36 @@ import NextIcon from 'components/shared/NextIcon';
 export default function ContactScene(props) {
   return (
     <ScrollView>
-      <View style={[zoneStyles.item, styles.item]}>
-        <Image style={zoneStyles.icon} source={require('assets/zone/qq.png')}/>
-        <Text style={zoneStyles.txt}>客服QQ交流群</Text>
-        <Text style={styles.linkTxt}>135784522</Text>
+      <View style={zoneStyles.group}>
+        <View style={[zoneStyles.item, styles.itemHeader]}>
+          <Image style={zoneStyles.icon} source={require('assets/zone/qq.png')}/>
+          <Text style={zoneStyles.txt}>客服QQ交流群</Text>
+        </View>
+        <View style={[zoneStyles.item, styles.itemBody]}>
+          <Text style={styles.linkTxt}>135784522</Text>
+        </View>
       </View>
 
-      <View style={[zoneStyles.item, styles.item]}>
-        <Image style={zoneStyles.icon} source={require('assets/zone/business.png')}/>
-        <Text style={zoneStyles.txt}>商务合作</Text>
-        <Text style={styles.linkTxt}>hejunxian01@chinatopcredit.com</Text>
+      <View style={zoneStyles.group}>
+        <View style={[zoneStyles.item, styles.itemHeader]}>
+          <Image style={zoneStyles.icon} source={require('assets/zone/business.png')}/>
+          <Text style={zoneStyles.txt}>商务合作</Text>
+        </View>
+        <View style={[zoneStyles.item, styles.itemBody]}>
+          <Text style={styles.linkTxt}>hejunxian01@chinatopcredit.com</Text>
+        </View>
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  item: {
-    borderBottomWidth: 0,
-    marginTop: 5,
-    height: 60
+  itemHeader: {
+    height: 40,
+  },
+  itemBody: {
+    paddingLeft: 45,
+    height: 52
   },
   linkTxt: {
     fontSize: 16,
