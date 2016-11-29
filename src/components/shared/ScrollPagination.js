84b400ec32b8b912015ba99e46fdc21b7bcc7e34
rollandlee <rolland.lee@gmail.com>
2016-11-29 18:09:46 +0800

    delete 正在加载 text

--- src/components/shared/ScrollPagination.js
+++ src/components/shared/ScrollPagination.js
@@ -53,7 +53,7 @@ export default class ScrollPagination extends PureComponent {
 
     return (
       <View style={styles.footer}>
-        <Loading/><Text style={{marginLeft: 5}}>正在努力加载...</Text>
+        <Loading/>
       </View>
     );
   }
