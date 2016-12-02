b1dc3a13670d90d61e46fac367a76c8c65cc2284
cattze <cattze@cattzedeMacBook-Pro.local>
2016-12-02 18:09:59 +0800

    fetched

--- src/reducers/scene/loanDetail.js
+++ src/reducers/scene/loanDetail.js
@@ -1,15 +1,16 @@
 const initState = {
   isFetching: true,
   fetchedParams: undefined,
-  detail: {}
+  detail: {},
+  fetched: false
 };
 
 export default function loanDetail(state = initState, action) {
   switch(action.type) {
     case 'requestLoanDetail':
-      return Object.assign({}, state, { isFetching: true });
+      return Object.assign({}, state, { isFetching: true , fetched: false});
     case 'receiveLoanDetail':
-      return Object.assign({}, state, { isFetching: false, fetchedParams: action.fetchedParams, detail: action.detail } )
+      return Object.assign({}, state, { isFetching: false, fetched: true, fetchedParams: action.fetchedParams, detail: action.detail } )
     default:
       return state
   }
