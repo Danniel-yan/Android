4ad07895c2e7e45197325f6da224af28106d0146
ysr <yanshirong01@chinatopcredit.com>
2017-04-05 10:25:46 +0800

    确认借款

--- src/components/shared/LoanAdjustRText.js
+++ src/components/shared/LoanAdjustRText.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { trackingScene } from 'high-order/trackingPointGenerator';
-//import { receiveRepaymentAmount } from 'actions/online/repayAmount';
-import { receiveAdjustApproveAmount } from 'actions/online/adjustApproveAmount';
 import {
     AppRegistry,
     View,
