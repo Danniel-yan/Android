200ad2d6ee0173546f4b08b0333dc6b5ed0473a1
王睆 <wanghuan01@chinatopcredit.com>
2017-05-04 19:30:42 +0800

    Format函数 扩展

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -6,11 +6,25 @@ function idNO(value) {
     return /^\d{17}[\d|x|X]$/.test(value)
 }
 
+function floatNum(num) {
+    return /^\d+(?:\.\d?)?$/.test(num) || /^\d+(?:\.\d+)?$/.test(num);
+}
+
 function maxNumFormater(maxNum) {
     return function (str) {
         var numStr = str.replace(/[^\d]/g, '');
         var num = Math.min(parseInt(numStr), maxNum) || 0;
         return num.toString();
+    };
+}
+
+function maxFloatFormater(maxNum) {
+    return function (str) {
+        if(!/^\d+(?:\.\d?)?$/.test(str) && !/^\d+(?:\.\d+)?$/.test(str)) return null;
+        var legalNumStr = str.replace(/[^\d\.]/g, '');
+        var num = Math.min(parseFloat(legalNumStr), maxNum) || 0;
+
+        return num != parseFloat(legalNumStr) ? num.toString() : legalNumStr;
     };
 }
 
