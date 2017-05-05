b832e8789a005d5e29fd69dad91c467f61872b4d
speakjan <speakjan@sina.cn>
2017-05-05 19:43:57 +0800

    startactivity

--- android/app/src/main/java/com/shudu/chaoshi/activity/WelcomeActivity.java
+++ android/app/src/main/java/com/shudu/chaoshi/activity/WelcomeActivity.java
@@ -23,7 +23,7 @@ public class WelcomeActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(WelcomeActivity.this, ImportBillChooseActivity.class));
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
                 finish();
             }
         }, 1000);
