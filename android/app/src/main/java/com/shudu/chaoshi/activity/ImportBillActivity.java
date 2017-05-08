209d3cf4798f8e7f688e3a08f46b894db43d292c
ysr <yanshirong01@chinatopcredit.com>
2017-05-08 18:07:33 +0800

    京东支付宝结果界面添加

--- android/app/src/main/java/com/shudu/chaoshi/activity/ImportBillActivity.java
+++ android/app/src/main/java/com/shudu/chaoshi/activity/ImportBillActivity.java
@@ -5,9 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.moxie.client.model.MxParam;
+import com.shudu.chaoshi.R;
 import com.shudu.chaoshi.util.Constants;
 
 import org.json.JSONObject;
@@ -17,16 +23,42 @@ import org.json.JSONObject;
  */
 
 public class ImportBillActivity extends Activity {
+
+    private ImageView iv_result_importbill;
+    private TextView tv_result_importbill, tv_result_importbill_title;
+    private Button btn_result_importbill;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        init();
+//        init();
+
+        setContentView(R.layout.activity_importbill_result);
+        initView();
+        resultData("支付宝账单导入", R.mipmap.ic_logo, "导入成功", View.VISIBLE);
+
+    }
+
+    private void initView() {
+        iv_result_importbill = (ImageView) findViewById(R.id.iv_result_importbill);
+        tv_result_importbill = (TextView) findViewById(R.id.tv_result_importbill);
+        btn_result_importbill = (Button) findViewById(R.id.btn_result_importbill);
+        tv_result_importbill_title = (TextView) findViewById(R.id.tv_result_importbill_title);
+    }
+
+    private void resultData(String title, int iv, String text, int isVisible) {
+        iv_result_importbill.setImageResource(iv);
+//        动态加载图
+//        Glide.with(this).load(R.drawable.loading).into(iv_result_importbill);
+        tv_result_importbill.setText(text);
+        btn_result_importbill.setVisibility(isVisible);
+        tv_result_importbill_title.setText(title);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (resultCode){
+        switch (resultCode) {
             case RESULT_OK:
 
                 Bundle b = data.getExtras();              //data为B中回传的Intent
@@ -48,7 +80,7 @@ public class ImportBillActivity extends Activity {
                  *      {"code" : 2, "function" : "mail", "searchId" : "3550622685459407187", "taskId" : "ce6b3806-57a2-4466-90bd-670389b1a112"}
                  */
 
-                if(TextUtils.isEmpty(result)) {
+                if (TextUtils.isEmpty(result)) {
                     Toast.makeText(ImportBillActivity.this, "用户没有进行导入操作!", Toast.LENGTH_SHORT).show();
                 } else {
                     try {
@@ -57,11 +89,11 @@ public class ImportBillActivity extends Activity {
                         JSONObject jsonObject = new JSONObject(result);
 
                         code = jsonObject.getInt("code");
-                        if(code == -1) {
+                        if (code == -1) {
                             Toast.makeText(ImportBillActivity.this, "用户没有进行导入操作!", Toast.LENGTH_SHORT).show();
-                        } else if(code == 0) {
+                        } else if (code == 0) {
                             Toast.makeText(ImportBillActivity.this, "导入失败!", Toast.LENGTH_SHORT).show();
-                        } else if(code == 1) {
+                        } else if (code == 1) {
                             switch (jsonObject.getString("function")) {
                                 case "alipay":
                                 case "taobao":
@@ -73,7 +105,7 @@ public class ImportBillActivity extends Activity {
                                     break;
                             }
                         }
-                    } catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -103,8 +135,8 @@ public class ImportBillActivity extends Activity {
 
     }
 
-    private void openWebViewResult(String str){
-        Log.d("123",str);
+    private void openWebViewResult(String str) {
+        Log.d("123", str);
         Intent intent = new Intent();
         intent.putExtra("urlStr", str);
         setResult(RESULT_OK, intent);
